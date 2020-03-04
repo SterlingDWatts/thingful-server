0b3a006684269e1c0f2c2ba70a94b6e37aa17736
Sterling Watts <SterlingDWatts@gmail.com>
2020-03-03 16:13:13 -0800

    Add bcrypt

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 function makeUsersArray() {
   return [
     {
@@ -221,28 +223,68 @@ function makeThingsFixtures() {
 }
 
 function cleanTables(db) {
-  return db.raw(
-    `TRUNCATE
-      thingful_things,
-      thingful_users,
-      thingful_reviews
-      RESTART IDENTITY CASCADE`
+  return db.transaction(trx =>
+    trx
+      .raw(
+        `TRUNCATE
+          thingful_things,
+          thingful_users,
+          thingful_reviews
+        `
+      )
+      .then(() =>
+        Promise.all([
+          trx.raw(
+            `ALTER SEQUENCE thingful_things_id_seq minvalue 0 START WITH 1`
+          ),
+          trx.raw(
+            `ALTER SEQUENCE thingful_users_id_seq minvalue 0 START WITH 1`
+          ),
+          trx.raw(
+            `ALTER SEQUENCE thingful_reviews_id_seq minvalue 0 START WITH 1`
+          ),
+          trx.raw(`SELECT setval('thingful_things_id_seq', 0)`),
+          trx.raw(`SELECT setval('thingful_users_id_seq', 0)`),
+          trx.raw(`SELECT setval('thingful_reviews_id_seq', 0)`)
+        ])
+      )
   );
 }
 
-function seedThingsTables(db, users, things, reviews = []) {
+function seedUsers(db, users) {
+  const preppedUsers = users.map(user => ({
+    ...user,
+    password: bcrypt.hashSync(user.password, 1)
+  }));
   return db
     .into("thingful_users")
-    .insert(users)
-    .then(() => db.into("thingful_things").insert(things))
-    .then(() => reviews.length && db.into("thingful_reviews").insert(reviews));
+    .insert(preppedUsers)
+    .then(() =>
+      //update the auto sequence to stay in sync
+      db.raw(`SELECT setval('thingful_users_id_seq', ?)`, [
+        users[users.length - 1].id
+      ])
+    );
+}
+
+function seedThingsTables(db, users, things, reviews = []) {
+  return db.transaction(async trx => {
+    await seedUsers(trx, users);
+    await trx.into("thingful_things").insert(things);
+    await trx.raw(`SELECT setval('thingful_things_id_seq', ?)`, [
+      things[things.length - 1].id
+    ]);
+    if (reviews.length) {
+      await trx.into("thingful_reviews").insert(reviews);
+      await trx.raw(`SELECT setval('thingful_reviews_id_seq', ?)`, [
+        reviews[reviews.length - 1].id
+      ]);
+    }
+  });
 }
 
 function seedMaliciousThing(db, user, thing) {
-  return db
-    .into("thingful_users")
-    .insert([user])
-    .then(() => db.into("thingful_things").insert([thing]));
+  return seedThingsTables(db, [user], [thing]);
 }
 
 function makeAuthHeader(user) {
@@ -264,5 +306,6 @@ module.exports = {
   cleanTables,
   seedThingsTables,
   seedMaliciousThing,
-  makeAuthHeader
+  makeAuthHeader,
+  seedUsers
 };
